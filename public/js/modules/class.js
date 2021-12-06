9e44c46c4c06adcd96f967045f3d873f3127aac6
Hndrckx <90184045+Hndrckx@users.noreply.github.com>
2021-12-06 14:42:03 +0100

    Working Methods Class

---
+++ public/js/modules/class.js
@@ -0,0 +1,32 @@
+class Pokemon{
+    constructor(nom, lvl, pdv, speed, type){
+        this.nom = nom
+        this.lvl = lvl
+        this.pdv = pdv
+        this.speed = speed
+        this.type = type
+    }
+}
+
+class Raichu extends Pokemon{
+    constructor(nom, lvl, pdv, speed, type, fatal, boule, vive, eclair){
+        super(nom, lvl, pdv, speed, type)
+        this.fatal = (a,b) =>
+        this.boule = (a,b) =>
+        this.vive = (a,b) =>
+        this.eclair = (a,b) =>
+    }      
+}
+
+class Dracolosse extends Pokemon{
+    constructor(nom, lvl, pdv, speed, type, danse, laser, vol, vampi){
+        super(nom, lvl, pdv, speed, type)
+        this.danse = (a,b) =>
+        this.laser = (a,b) =>
+        this.vol = (a,b) =>
+        this.vampi = (a,b) => 
+    }
+}
+
+let raichu = new Raichu ("Raichu", 54, 100, 80, ["Electrique", "Fée"])
+let dracolosse = new Dracolosse ("Dracolosse", 57, 100, 65, ["Dragon"])
